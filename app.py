fee3511bb02e71f8c60aab4f4bbed10d018d006e
Zach Johnson <ztjohns@icloud.com>
2023-01-26 17:40:19 -0700

    Update login query url

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ def index():
 
 @app.route('/login')
 def login():
-    return redirect('https://accounts.spotify.com/authorize?')
+    client_id = '45145719fea4498bb99238355a19bf3c'
+    redirect_uri = 'http://localhost:8888/callback'
+    state = 'Aa0H6frT44j9mMmM9a'
+    return redirect(f'https://accounts.spotify.com/authorize?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&state={state}')
 
 
 # @app.route('/redirect')
