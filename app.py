76a5f5f0a4d29fe320d62ae9c4889d140f7cde07
Zach Johnson <ztjohns@icloud.com>
2023-01-29 02:09:18 -0700

    authorization final

--- app.py
+++ app.py
@@ -11,12 +11,25 @@ client_id = 'e9e658d5ab0647c5b2979a9b0dccea05'
 redirect_uri = 'https://librarian-for-spotify.onrender.com/callback'
 client_secret = '1772fc32f7f0486b883e3f3f3911f358'
 
+access_token = ''
+logged_in = False
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if logged_in == False:
+        return redirect('/login')
+    else:
+        header = {'Authorization':f'Bearer{access_token}'}
+        response = requests.post('https://api.spotify.com/v1/me',headers=header).json()
+        # return render_template('index.html')
+        return response
 
 @app.route('/login')
 def login():
+    return render_template('login.html')
+
+@app.route('/auth')
+def auth():
     return redirect(f'https://accounts.spotify.com/authorize?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}')
 
 
@@ -32,13 +45,10 @@ def callback():
         'grant_type': 'authorization_code'
     }
     print(code)
-    body = requests.post('https://accounts.spotify.com/api/token',data=data)
-    return body.json()
-    
-
-
-
-# @app.route()
+    body = requests.post('https://accounts.spotify.com/api/token',data=data).json()
+    access_token = body['access_token']
+    logged_in = True
+    return redirect('/')  
 
 if __name__ == '__main__':
     app.run()
