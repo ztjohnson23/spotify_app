f9a8ce84a601b64dd0d678ea0c33047780cbb79b
Zach Johnson <ztjohns@icloud.com>
2023-01-30 15:21:12 -0700

    session debug 2

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 import flask
 from flask import Flask, render_template, redirect, request, jsonify, session
 from flask_cors.extension import CORS
+from flask.ext.session import Session
 import requests
 
 
 app = Flask(__name__)
 cors = CORS(app)
+Session(app)
 
 client_id = 'e9e658d5ab0647c5b2979a9b0dccea05'
 redirect_uri = 'https://librarian-for-spotify.onrender.com/callback'
 client_secret = '1772fc32f7f0486b883e3f3f3911f358'
 
-access_token = ''
 
 @app.route('/')
 def index():
@@ -36,6 +37,8 @@ def callback():
     }
     body = requests.post('https://accounts.spotify.com/api/token',data=data).json()
     access_token = body['access_token']
+    print(f'p1 - token is:"{access_token}"')
+
     session['access_token'] = access_token
     return redirect('/home')  
 
@@ -43,7 +46,7 @@ def callback():
 @app.route('/home')
 def home():
     access_token = session.get('access_token')
-    print(access_token)
+    print(f'p2 - token is:"{access_token}"')
     header = {'Authorization':f'Bearer {access_token}'}
     response = requests.post('https://api.spotify.com/v1/me',headers=header).json()
     # return render_template('index.html')
