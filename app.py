97530146f97b67a29f50bc0795baebe7689abc76
Zach Johnson <ztjohns@icloud.com>
2023-02-22 18:27:46 -0700

    missing parentheses

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def callback():
     #############################################
     header = {'Authorization':f'Bearer {access_token}'}
     body = requests.post('https://api.spotify.com/v1/recommendations/available-genre-seeds',headers=header)
-    print(body.status_code
+    print(body.status_code)
     print(body.reason))
     print('Text is: ', body.text)
     print(type(body))
