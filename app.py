278830d2ce949d309ff1f49b1bbc271620fc6554
Zach Johnson <ztjohns@icloud.com>
2023-06-08 22:14:57 -0700

    back to flask

--- app.py
+++ app.py
@@ -4,15 +4,10 @@ from flask_cors.extension import CORS
 # from flask_session import Session
 import requests
 
-
 app = Flask(__name__)
 cors = CORS(app)
 
-# app.secret_key = 'g43tjbofq0'
-# SESSION_TYPE = 'redis'
-# app.config['SECRET_KEY']='sxZZZ1234'
-# app.config.from_object(__name__)
-# Session(app)
+app.secret_key = 'itsasecret123123'
 
 client_id = 'e9e658d5ab0647c5b2979a9b0dccea05'
 redirect_uri = 'https://librarian-for-spotify.onrender.com/home'
@@ -46,37 +41,14 @@ def callback():
     body = requests.post('https://accounts.spotify.com/api/token',data=data).json()
     access_token = body['access_token']
 
-   
-    # session['access_token'] = access_token
-    #############################################
-    # header = {'Authorization':f'Bearer {access_token}'}
-    # body = requests.get('https://api.spotify.com/v1/me',headers=header).json()
-
-    # username = body['display_name']
-
-    # body = requests.get('https://api.spotify.com/v1/me/tracks',headers=header,params={'limit':50}).json()
-    #         # ,data={'limit':50}
-    # user_songs = body['items']
-    # i=1
-    # while body['next'] != None:
-    #     body = requests.get(body['next'],params={'limit':50},headers=header).json()
-    #     user_songs.extend(body['items'])
-    #     print(f'{i}/63'); i+=1
-
-
     return render_template('home.html',token=access_token,body=body)
 
 
+@app.route('/run', methods = ['POST'])
+def run():
+    track_data = request.form['data']
+    return jsonify(track_data)
 
-# @app.route('/home')
-# def home():
-#     print('p3 - start of home route')
-#     access_token = session.get('access_token')
-#     print(f'p4 - token is:"{access_token}. Success"')
-#     header = {'Authorization':f'Bearer {access_token}'}
-#     response = requests.post('https://api.spotify.com/v1/recommendations/available-genre-seeds',headers=header).json()
-#     # return render_template('index.html')
-#     return response
 
 
 if __name__ == '__main__':
