4e4c86cd514fb97357f8f7407dfc9a4ae518468b
Zach Johnson <ztjohns@icloud.com>
2023-06-09 02:09:45 -0700

    attempt with sessions

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import flask
-from flask import Flask, render_template, redirect, request, jsonify, Response, url_for
+from flask import Flask, render_template, redirect, request, jsonify, Response, session
 from flask_cors.extension import CORS
 # from flask_session import Session
 import requests
@@ -46,32 +46,45 @@ def callback():
     return render_template('home.html',token=access_token,body=body)
 
 
-@app.route('/run', methods = ['GET','POST'])
+@app.route('/run', methods = ['POST'])
 def run():
-    if request.method == 'POST':
-        track_data = json.loads(request.form['data'])
-        tracks_df = pd.DataFrame(track_data)
-        return redirect('/run')
-    else:
-        file_buffer = StringIO()
-        tracks_df.to_csv(file_buffer)
-        file_buffer.seek(0)
-        response = Response(file_buffer,mimetype='text/csv')
-        response.headers.set("Content-Disposition", "attachment", filename="usertracks.csv")
-        return response
-
+    # track_data = json.loads(request.form['data'])
+    session['data'] = request.form['data']
+    return
     # return jsonify(track_data)
+    
+    # tracks_df = pd.DataFrame(track_data)
+    # file_buffer = StringIO()
+    # tracks_df.to_csv(file_buffer)
+    # file_buffer.seek(0)
+    # response = Response(file_buffer,mimetype='text/csv')
+    # response.headers.set("Content-Disposition", "attachment", filename="usertracks.csv")
+    # return response
+
 
 @app.route('/test')
 def getfile():
-    test_data = {'c1':[0,1,2,3,4],'c2':[1,2,3,4,5],'c3':['a','b','c','d','e']}
-    df = pd.DataFrame(test_data)
-    file = StringIO()
-    df.to_csv(file)
-    file.seek(0)
-    response=Response(file,mimetype='text/csv')
-    response.headers.set("Content-Disposition", "attachment", filename="test.csv")
+    track_data = session['data']
+    tracks_df = pd.DataFrame(track_data)
+    file_buffer = StringIO()
+    tracks_df.to_csv(file_buffer)
+    file_buffer.seek(0)
+    response = Response(file_buffer,mimetype='text/csv')
+    response.headers.set("Content-Disposition", "attachment", filename="usertracks.csv")
     return response
+
+
+
+
+
+    # test_data = {'c1':[0,1,2,3,4],'c2':[1,2,3,4,5],'c3':['a','b','c','d','e']}
+    # df = pd.DataFrame(test_data)
+    # file = StringIO()
+    # df.to_csv(file)
+    # file.seek(0)
+    # response=Response(file,mimetype='text/csv')
+    # response.headers.set("Content-Disposition", "attachment", filename="test.csv")
+    # return response
 
 if __name__ == '__main__':
     app.run()
